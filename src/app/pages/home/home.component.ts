d240b827e480b69eb29f0ef4b8405712090ef2c2
milleradriano <milleradriano@gmail.com>
2024-01-05 09:32:28 -0300

    Ajuste para retornar os valores do principal direto do datafake

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { dataFake } from '../../data/dataFake';
+dataFake;
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  photoCover: string = '';
+  contentTitle: string = '';
+  contentDescription: string = '';
+  articles: any[] = [''];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private route: ActivatedRoute) {}
+  private id: any = '0';
+  ngOnInit(): void {  
+    for (let i = 1; i < dataFake.length; i++) {           
+      this.articles = dataFake.filter(article => article.id > 1);
+    }
   }
-
 }
